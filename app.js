46a9bfa8b5b7c279cabbcbd67c499e22f17045d1
Kapil1721 <121785696+sahilegss@users.noreply.github.com>
2023-01-02 12:12:49 +0530

    change

---
+++ app.js
@@ -0,0 +1,34 @@
+// server/index.js
+
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    methods: ["GET", "POST"],
+  },
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json("working");
+});
+
+io.on("connection", (socket) => {
+  console.log("New Message");
+
+  socket.on("message", (data) => {
+    socket.broadcast.emit("receiveMsg", data);
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", " A user  left the chat");
+  });
+});
+
+server.listen(5000, () => "Server is running on port 3000");
